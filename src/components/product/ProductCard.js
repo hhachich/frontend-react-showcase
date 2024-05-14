8c98a9c09c7e91de39b535ed2bd3a20174bb5a2e
hhachich <h.hachichou@gmail.com>
2024-05-14 11:04:26 +0200

    condition affichage pour les images

--- src/components/product/ProductCard.js
+++ src/components/product/ProductCard.js
@@ -3,12 +3,16 @@ import { Link } from "react-router-dom";
 
 function ProductCard({ product, id }) {
   console.log(id)
+  console.log(product)
   return (
     <>
       <div className="col s12 m3">
         <div className="card">
           <div className="card-image">
-            <img alt={product.image.data.attributes.title} src={process.env.REACT_APP_APP_URL + product.image.data.attributes.url} />
+            {product.image.data
+              ? <img alt={product.image.data.attributes.title} src={process.env.REACT_APP_APP_URL + product.image.data.attributes.url} />
+              : <p>image not found</p>
+            }
           </div>
           <div className="card-content">
             <span className="card-title" >{product.title}</span>
